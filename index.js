232c72542cf5fbe9155068f506b194d5ab93acf3
Ridampreet Jaggi <ridam.preet@gmail.com>
2022-03-07 17:07:44 -0400

    push

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require("./app");
 
 const port = 8000 || global.process.env.PORT;
 const server = http.createServer(app);
-server.listen(port, () =>
+server.listen(process.env.PORT || 8000, () =>
   console.log("Server has been started.... on port " + port)
 );
